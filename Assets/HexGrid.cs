7a8c36ddc65b8559b0f824b7f4795d557712b53b
yberry <yanisberry16@gmail.com>
2018-02-08 16:00:26 +0100

    Start

---
+++ Assets/HexGrid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+    public Text cellLabelPrefab;
+
+    HexCell[] cells;
+    Canvas gridCanvas;
+
+    private void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position = new Vector3(
+            (x + z * 0.5f - z / 2) * 2f * HexMetrics.innerRadius,
+            0f,
+            z * 1.5f * HexMetrics.outerRadius
+        );
+
+        HexCell cell = cells[i] = Instantiate(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+
+        Text label = Instantiate(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = x.ToString() + "\n" + z.ToString();
+    }
+}
---
+++ Assets/HexMesh.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour {
+
+    Mesh hexMesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+}
