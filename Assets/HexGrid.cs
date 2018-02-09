31e7ca57f7999511cf89c76205015529725803e5
yberry <yanisberry16@gmail.com>
2018-02-09 14:40:08 +0100

    Tab 4

--- Assets/HexGrid.cs
+++ Assets/HexGrid.cs
@@ -12,12 +12,21 @@ public class HexGrid : MonoBehaviour {
     public HexCell cellPrefab;
     public Text cellLabelPrefab;
 
+    public Texture2D noiseSource;
+
     HexCell[] cells;
     Canvas gridCanvas;
     HexMesh hexMesh;
 
+    private void OnEnable()
+    {
+        HexMetrics.noiseSource = noiseSource;
+    }
+
     private void Awake()
     {
+        HexMetrics.noiseSource = noiseSource;
+
         gridCanvas = GetComponentInChildren<Canvas>();
         hexMesh = GetComponentInChildren<HexMesh>();
 
@@ -81,6 +90,7 @@ public class HexGrid : MonoBehaviour {
         label.text = cell.coordinates.ToStringOnSeparateLines();
 
         cell.uiRect = label.rectTransform;
+        cell.Elevation = 0;
     }
 
     public HexCell GetCell(Vector3 position)
--- Assets/HexMetrics.cs
+++ Assets/HexMetrics.cs
@@ -9,13 +9,18 @@ public static class HexMetrics {
 
     public const float outerRadius = 10f;
     public const float innerRadius = outerRadius * 0.866025404f;
-    public const float solidFactor = 0.75f;
+    public const float solidFactor = 0.8f;
     public const float blendFactor = 1f - solidFactor;
-    public const float elevationStep = 5f;
+    public const float elevationStep = 3f;
     public const int terracesPerSlope = 2;
     public const int terraceSteps = terracesPerSlope * 2 + 1;
     public const float horizontalTerraceStepSize = 1f / terraceSteps;
     public const float verticalTerraceStepSize = 1f / (terracesPerSlope + 1);
+    public const float cellPerturbStrength = 4f;
+    public const float noiseScale = 0.003f;
+    public const float elevationPerturbStrength = 1.5f;
+
+    public static Texture2D noiseSource;
 
     static Vector3[] corners =
     {
@@ -82,5 +87,10 @@ public static class HexMetrics {
             return HexEdgeType.Slope;
         }
         return HexEdgeType.Cliff;
+    }
+
+    public static Vector4 SampleNoise(Vector3 position)
+    {
+        return noiseSource.GetPixelBilinear(position.x * noiseScale, position.z * noiseScale);
     }
 }
